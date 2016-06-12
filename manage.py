60c0c01dd9aac16f9e032fe6a63b726353103f27
Madhav <madhavkapoor1990@gmail.com>
2016-06-12 13:12:04 +0530

    Taskbuster project created

---
+++ manage.py
@@ -0,0 +1,11 @@
+#!/home/madhav/.virtualenvs/tb_dev/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    #print(os.environ.get("SAMPLE"))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskbuster.settings.development")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
